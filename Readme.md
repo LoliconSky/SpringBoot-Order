072a1f09407a22ba58766c9759db7a9b433bb8cb
bfchengnuo <bfchengnuo@gmail.com>
2019-03-10 21:11:09 +0800

    修改之前错误的@DynamicUpdate说明； 增加 Readme

---
+++ Readme.md
@@ -0,0 +1,25 @@
+基于 SpringBoot 的微信点餐系统
+
+分为卖家端（Web 管理后台）和买家端（微信端，使用 Vue）
+
+- 日志的配置与使用
+- @DynamicUpdate使用
+- SpringDataJPA
+
+## 分页
+
+示例代码：
+``` java
+PageRequest pageRequest = PageRequest.of(0, 2);
+Page<ProductInfo> page = productService.findAll(pageRequest);
+System.out.println(page.getTotalElements());
+Assert.assertNotEquals(0, page.getTotalElements());
+
+
+// 创建排序规则
+Sort sort = new Sort(Sort.Direction.DESC, "id");
+// 创建分页对象
+Pageable pageable = new PageRequest(pageIndex, pageSize, sort);
+```
+
+其中，findAll 是已经默认实现的方法，接收一个 Pageable 类型的分页参数，PageRequest 间接实现了 Pageable
--- src/main/java/com/bfchengnuo/sell/po/ProductCategory.java
+++ src/main/java/com/bfchengnuo/sell/po/ProductCategory.java
@@ -9,13 +9,18 @@ import javax.persistence.Id;
 
 /**
  * 对于 @DynamicUpdate 的说明：
- * 如果我们在更新表时,只想更新某个字段,就不要加 @DynamicUpdate,通常为了更新表时的效率,都是不加的.
- * 反之,如果我们更新某个字段时,更新所有的字段,就可以加上 @DynamicUpdate.
- * DynamicInsert 同理
+ * 如果我们在更新表时,只想更新某个修改了的字段,就加 @DynamicUpdate,通常为了提高更新表时的效率.
+ * 它的作用是更新“变化”的字段
+ * 反之,如果我们更新某个字段时,更新所有的字段,就可以加上 @DynamicUpdate(false). 默认值是 true，不加注解相当于 false
+ * DynamicInsert 同理（插入不为空的列）
  *
  * 本例中，如果增加了 updateTime 字段，则需要使用 DynamicUpdate 注解配合数据设置的
  *      DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
- *      来由数据库自动更新这个更新时间，因为默认 updateTime 不设置值或者不变的话，sql 是不会拼上的，数据库设置的规则也就无效了
+ *      来由数据库自动更新这个更新时间，因为 updateTime 明确使用 sql 更新的情况下，数据库设置的规则也就无效了
+ *
+ * 然而，当我们使用dynamic-insert、dynamic-update时, Hibernate都要重新计算一次相应的SQL语句，在Hibernate层面会有性能损耗。
+ * 只有表中有庞大的blog列或者有超多的列，dynamic insert、 dynamic update才有意义；否则我不认为它们会带来性能提升。
+ *
  * Created by 冰封承諾Andy on 2018/7/17.
  */
 @Entity
