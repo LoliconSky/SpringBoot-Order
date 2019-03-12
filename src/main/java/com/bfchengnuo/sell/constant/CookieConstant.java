aaa128d6fccb1e19191a79d96eb7dc0dd168079b
bfchengnuo <bfchengnuo@gmail.com>
2019-03-12 15:07:43 +0800

    用户登陆登出

---
+++ src/main/java/com/bfchengnuo/sell/constant/CookieConstant.java
@@ -0,0 +1,8 @@
+package com.bfchengnuo.sell.constant;
+
+public interface CookieConstant {
+
+    String TOKEN = "token";
+
+    Integer EXPIRE = 7200;
+}
---
+++ src/main/java/com/bfchengnuo/sell/constant/RedisConstant.java
@@ -0,0 +1,9 @@
+package com.bfchengnuo.sell.constant;
+
+public interface RedisConstant {
+
+    String TOKEN_PREFIX = "token_%s";
+
+    // 2小时
+    Integer EXPIRE = 7200;
+}
